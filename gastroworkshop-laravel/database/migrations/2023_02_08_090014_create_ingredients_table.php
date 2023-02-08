ed35a15a63bb09ed0e886811d63f682afa4654aa
szogipeti <szogipeti02@gmail.com>
2023-02-08 11:10:48 +0100

    Add fields to migration

--- gastroworkshop-laravel/database/migrations/2023_02_08_090014_create_ingredients_table.php
+++ gastroworkshop-laravel/database/migrations/2023_02_08_090014_create_ingredients_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('ingredients', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 25);
+            $table->string('category', 25);
             $table->timestamps();
         });
     }
