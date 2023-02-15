3dc47e95d7b7e146faf8da8194997bd92369957c
denesdonat <donatking2002@gmail.com>
2023-02-15 08:29:01 +0100

    Measure seeder

--- Seeders/MeasureSeeder.php
+++ Seeders/MeasureSeeder.php
@@ -14,8 +14,22 @@ class CsapattagSeeder extends Seeder
      */
     public function run()
     {
-        DB::table("Ingredient")->insert(
+        DB::table("Measure")->insert(
             [
+			['id' => '1','name' => 'tablespoon'],
+			['id' => '2','name' => 'teaspoon'],
+			['id' => '3','name' => 'ounce'],
+			['id' => '4','name' => 'fluid ounce'],
+			['id' => '5','name' => 'cup'],
+			['id' => '6','name' => 'quart'],
+			['id' => '7','name' => 'pint'],
+			['id' => '8','name' => 'gallon'],
+			['id' => '9','name' => 'pound'],
+			['id' => '10','name' => 'milliliter'],
+			['id' => '11','name' => 'grams'],
+			['id' => '12','name' => 'kilogram'],
+			['id' => '13','name' => 'liter'],
+			['id' => '14','name' => 'milliliter']
             ]);
     }
 }
