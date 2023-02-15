bca27b10febe50698f6da14255e845389ab90ae9
szogipeti <szogipeti02@gmail.com>
2023-02-15 09:22:31 +0100

    Restore main.blade.php

--- gastroworkshop-laravel/resources/views/layouts/main.blade.php
+++ gastroworkshop-laravel/resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield("title")</title>
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     @include('layouts.menu')
