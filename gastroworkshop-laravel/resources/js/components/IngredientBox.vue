f36e51dcec83613e092074df5568d232875197c4
Szögi Péter <szogipeti02@gmail.com>
2023-02-27 10:45:11 +0100

    Create header for ingredientBox

--- gastroworkshop-laravel/resources/js/components/IngredientBox.vue
+++ gastroworkshop-laravel/resources/js/components/IngredientBox.vue
@@ -1,30 +1,41 @@
 <template>
     <div class="ingredient-box container">
-        <div class="row">
-            <div class="input col-4 p-0">
-                <v-select placeholder="Ingredient" id="ingredient" v-bind="this.selectedIngredient" label="name" :options="ingredients" />
+        <div class="row new-ingredient">
+            <div class="input col-12 col-md-6 p-0">
+                <select data-live-search="true" class="form-control" name="ingredient" id="ingredient" v-bind:value="this.selectedIngredient">
+                    <option value="" disabled selected>Ingredient</option>
+                    <option v-for="ingredient in ingredients" :key="ingredient.id" value="{{ingredient.id}}">{{ingredient.name}}</option>
+                </select>
+                <!--<v-select class="form-control" placeholder="Ingredient" id="ingredient" v-bind="this.selectedIngredient" label="name" :options="ingredients" />-->
             </div>
-            <div class="input col-2 p-0">
+            <div class="input col-12 col-md-6 p-0">
                 <input placeholder="Quantity" class="form-control" type="number" name="quantity" id="quantity">
             </div>
-            <div class="input col-3 p-0">
-                <v-select placeholder="Measure" id="measure" label="name" :options="measures.filter((x) => {return x.name === this.selectedIngredient})" />
+            <div class="input col-12 col-md-6 p-0">
+                <select data-live-search="true" class="form-control" name="ingredient" id="ingredient" v-bind:value="this.selectedMeasure">
+                    <option value="" disabled selected>Measure</option>
+                    <option v-for="measure in measures.filter((x) => {return x.name === this.selectedIngredient})" :key="measure.id" value="{{measure.id}}">{{measure.name}}</option>
+                </select>
+                <!--<v-select class="form-control" placeholder="Measure" id="measure" label="name" :options="measures.filter((x) => {return x.name === this.selectedIngredient})" />-->
             </div>
-            <div class="input col-3 p-0">
-                <button class="btn btn-success">Add ingredient</button>
+            <div class="input col-12 col-md-6 p-0">
+                <button class="btn btn-secondary w-100">Add ingredient</button>
             </div>
         </div>
-        <ul>
-            <li v-for="ingredient in ownedIngredients" :key="ingredient.id">
-                {{ingredient.name}} - {{ingredient.quantity===null?ingredient.measure:ingredient.quantity + " " + ingredient.measure + "(s)"}}
-            </li>
-        </ul>
+        <div class="row">
+            <div class="col">
+                <ul>
+                    <li v-for="ingredient in ownedIngredients" :key="ingredient.id">
+                        {{ingredient.name}} - {{ingredient.quantity===null?ingredient.measure:ingredient.quantity + " " + ingredient.measure + "(s)"}}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import vSelect from 'vue-select';
-import 'vue-select/dist/vue-select.css'
 export default{
     components:{
         vSelect
@@ -33,7 +44,8 @@ export default{
         return{
             ingredients: [],
             measures: [],
-            selectedIngredient: null
+            selectedIngredient: null,
+            selectedMeasure: null
         }
     },
     props:{
@@ -67,7 +79,11 @@ export default{
     margin-bottom: 20px;
     font-size: .75rem;
 }
-.row input, .v-select, .vs__dropdown-toggle, .vs__selected-options{
-    height: 60px;
+.form-control, button{
+    height: 40px;
+    border-radius: 0;
+}
+.new-ingredient{
+    border-bottom: 1px solid black;
 }
 </style>
