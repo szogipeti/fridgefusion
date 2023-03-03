0fa2a7438d167127ecf784c349e8a260baaeeaf2
szogipeti <szogipeti02@gmail.com>
2023-03-03 13:40:29 +0100

    Replace navbar with a bootstrap one

--- gastroworkshop-laravel/resources/js/components/NavBar.vue
+++ gastroworkshop-laravel/resources/js/components/NavBar.vue
@@ -1,39 +1,40 @@
 <template>
-    <nav class="nav">
-        <a href="#" class="nav-brand">Fridge Fusion</a>
-        <ul class="nav-menu" v-bind:class="{active: menuActive}">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Seasonal recipes</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Weekly recipes</a>
-            </li>
-        </ul>
-        <div class="d-flex align-items-center ms-auto ms-md-0">
-                <router-link to="/login" class="nav-link">Login</router-link>
-            <router-link to="/profile">
-            <p class="mx-1 my-auto mr-2 text-white">Your profile</p>
-            <img class="mx-1" src="/img/profile-icon.png" alt="">
-            </router-link>
-        </div>
-        <div class="hamburger mx-2" v-bind:class="{active: menuActive}" @click="toggleMenu">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white" href="#">Fridge Fusion</a>
+            <div class="order-lg-1 d-flex justify-content-end align-items-center">
+                <div class="d-flex align-items-center ms-auto ms-md-0">
+                    <router-link to="/login" class="nav-link">Login</router-link>
+                    <router-link to="/profile" class="d-flex justify-content-end align-items-center">
+                        <p class="mx-1 my-auto mr-2 text-white">Your profile</p>
+                        <img class="mx-1" src="/img/profile-icon.png" alt="">
+                    </router-link>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse order-0" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Seasonal recipes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Weekly recipes</a>
+                    </li>
+                </ul>
+            </div>
+
+
         </div>
     </nav>
 </template>
 
 <script setup>
-import {ref} from "vue";
 
-const menuActive = ref(false);
-const toggleMenu = function(){
-    menuActive.value = !menuActive.value;
-}
 </script>
 
 <style scoped>
@@ -53,100 +54,22 @@ a{
     text-decoration: none;
 }
 
-.hamburger{
-    display: none;
-}
-
-.bar{
-    display: block;
-    width: 25px;
-    height: 3px;
-    margin: 5px auto;
-    -webkit-transition: all 0.3s ease-in-out;
-    transition: all 0.3s ease-in-out;
-    background-color: white;
-}
-
-.nav{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0.5rem 0.25rem;
-}
-
-.nav-menu{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0;
-    margin: 0;
-}
-
 .nav-item{
     margin: 0 10px;
 }
 
-.nav-link, .nav-link:link, .nav-link:visited, .nav-link:hover, .nav-link:active{
+.nav-link, .navbar-nav .show > .nav-link, .navbar-nav .nav-link.active, .nav-link:hover{
     color: white;
 }
+.nav-link:focus{
+    color: #ddd;
+}
 
-.nav-brand{
+.navbar-brand{
     font-size: 2rem;
     font-weight: 500;
     color: white;
     margin: 0 10px;
 }
 
-@media only screen and (max-width: 768px){
-    .nav-menu{
-        position: fixed;
-        left: -100%;
-        top: calc(40px + 1.5rem);
-        flex-direction: column;
-        width: 100%;
-        text-align: center;
-        transition: left 0.3s;
-        box-shadow: 0 10px 27px rgba(0,0,0,0.05);
-        background-color: white
-    }
-
-    .nav-menu.active{
-        left: 0;
-        box-shadow: 0 0 0 max(100vh, 100vw) rgba(0, 0, 0, .3);
-        -moz-transition: box-shadow 200ms;
-        -webkit-transition: box-shadow 200ms;
-        transition: box-shadow 200ms, left 200ms;
-        position: sticky;
-    }
-
-    .nav-item{
-        width: 100%;
-    }
-
-    .nav-link, .nav-link:link, .nav-link:visited, .nav-link:active{
-        color: black;
-    }
-
-    .nav-item:hover{
-        background-color: #56b1ab;
-        transition: 0.3s
-    }
-
-    .hamburger{
-        display: block;
-        cursor: pointer;
-    }
-
-    .hamburger.active .bar:nth-child(2) {
-        opacity: 0;
-    }
-
-    .hamburger.active .bar:nth-child(1) {
-        transform: translateY(8px) rotate(45deg);
-    }
-
-    .hamburger.active .bar:nth-child(3) {
-        transform: translateY(-8px) rotate(-45deg);
-    }
-}
 </style>
