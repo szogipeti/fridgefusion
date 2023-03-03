2d604dcbb4f2c1f9489c94e0423592bb92c6fc38
denesdonat <donatking2002@gmail.com>
2023-03-03 21:39:18 +0100

    profile page

--- gastroworkshop-laravel/resources/js/views/Profile.vue
+++ gastroworkshop-laravel/resources/js/views/Profile.vue
@@ -2,24 +2,29 @@
     <div>
        <Form>
         <h1>Profile</h1>
-        <p>Username: {{ user.username }}</p>
-        <p>Email: {{ user.email }}</p>
-        <p>Password: {{user.password}}</p>
+        <p>Username: {{ userData.username }}</p>
+        <p>Email: {{ userData.email }}</p>
+        <p>Password: {{ userData.password}}</p>
         <label for="new_recipe">Make new Recipe</label>
         <Field type="recipe" name="email" class="form-control"/>
        </Form>
     </div>
 </template>
 
-<script>
+<script setup>
 import { reactive, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { Form, Field, ErrorMessage } from 'vee-validate';
 import * as yup from 'yup';
-import axios from 'axios';
+import axios, {Axios} from 'axios';
 
-function User()
-{
-    return Auth::user();
+const router = useRouter();
+    async function getUserData(){
+    const  response = await http.get("users");
+    userData.username = response.data.username;
+    userData.email = response.data.email;
+    userData.password = response.data.password;
 }
+
+getUserData();
 </script>
