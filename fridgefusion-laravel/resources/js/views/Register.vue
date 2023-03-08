7d8e19b13d94e9a9acabbc958c0dab8aab662c41
vastikrisz <vastikrisz@gmail.com>
2023-03-08 20:52:56 +0100

    finished register page design and added btn back to login

--- fridgefusion-laravel/resources/js/views/Register.vue
+++ fridgefusion-laravel/resources/js/views/Register.vue
@@ -3,7 +3,6 @@
         <h3>Create your Fridge Fusion account</h3>
         <Form @submit="register" :validation-schema="Schema">
             <div class="form-group">
-<<<<<<< HEAD
                 <Field type="string" placeholder=" Username" name="username"/>
                 <div class="errdiv"><error-message name="username"></error-message></div>
             </div>
@@ -18,25 +17,10 @@
             <div class="form-group mt-3">
                 <Field type="password" placeholder=" Confirm Password" name="password_confirmation"/>
                 <div class="errdiv"><error-message name="password"></error-message></div>
-=======
-                <Field type="string" placeholder="Username" name="username" class="form-control error-message-email"/>
-                <error-message class="error-message mt-3" name="username"></error-message>
-            </div>
-            <div class="form-group mt-3">
-                <Field type="email" placeholder="E-mail" name="email" class="form-control error-message-email"/>
-                <error-message class="error-message mt-3" name="email"></error-message>
-            </div>
-            <div class="form-group mt-3">
-                <Field type="password" placeholder="Password" name="password" class="form-control"/>
-                <error-message class="error-message mt-3" name="password"></error-message>
-            </div>
-            <div class="form-group mt-3">
-                <Field type="password" placeholder="Confirm Password" name="password_confirmation" class="form-control"/>
-                <error-message class="error-message mt-3" name="password"></error-message>
->>>>>>> 620813bf409859404fc973734bfe500bae1ca02b
             </div>
             <input type="submit" value="Register" class="mt-3">
         </Form>
+        <h5>Already have an account? <router-link to="/login"><label id="labellog">Login here!</label></router-link></h5>
     </div>
 </template>
 <script setup>
@@ -93,46 +77,36 @@ input{
 #regdiv{
     margin-top: 30px;
     margin-bottom: 40px;
-    background-color: whitesmoke;
+    background-color: white;
     border: 2px solid black;
     box-shadow:10px 10px #18afa5;
 }
 h3{
     text-align: center;
 }
-<<<<<<< HEAD
 input{
     padding-top: 10px;
     padding-bottom: 10px;
-    border: 2px solid black;
+    border: 2px solid #18afa5;
 }
 input:focus{
-    background-color: lightblue;
-    border: 3px solid black;
+    background-color: #18afa5;
+    border: 3px solid #18afa5;
 }
 input[type=submit]{
     width: 70%;
     margin-left: 15%;
+    border: 3px solid #18afa5;
+}
+input[type=submit]:hover{
+    background-color: #18afa5;
 }
 .errdiv{
     margin-left: auto;
     margin-right: auto;
     text-align: center;
-=======
-.error-message{
-    position: absolute;
-    top: calc(100% + 5px);
-    left: 0;
-    width: 100%;
-    text-align: center;
-    margin-top: 5px;
 }
-.form-group {
-    position: relative;
-    margin-bottom: 1.5rem;
-}
-.error-message-email{
-    margin-top: 10px;
->>>>>>> 620813bf409859404fc973734bfe500bae1ca02b
+#labellog{
+    cursor: pointer;
 }
 </style>
