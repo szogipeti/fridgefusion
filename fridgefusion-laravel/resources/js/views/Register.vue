6ece83f0aef224d623ee930e3de15d9c194ca0da
vastikrisz <vastikrisz@gmail.com>
2023-03-08 19:28:19 +0100

    register page design almost finished

--- fridgefusion-laravel/resources/js/views/Register.vue
+++ fridgefusion-laravel/resources/js/views/Register.vue
@@ -1,24 +1,24 @@
 <template>
-    <div id="regdiv" class="bg-opacity-50 mx-auto p-3 rounded">
+    <div id="regdiv" class="bg-opacity-50 mx-auto p-3">
         <h3>Create your Fridge Fusion account</h3>
         <Form @submit="register" :validation-schema="Schema">
             <div class="form-group">
-                <Field type="string" placeholder="Username" name="username" class="form-control"/>
-                <error-message name="username"></error-message>
+                <Field type="string" placeholder=" Username" name="username"/>
+                <div class="errdiv"><error-message name="username"></error-message></div>
             </div>
             <div class="form-group mt-3">
-                <Field type="email" placeholder="E-mail" name="email" class="form-control"/>
-                <error-message name="email"></error-message>
+                <Field type="email" placeholder=" E-mail" name="email"/>
+                <div class="errdiv"><error-message name="email"></error-message></div>    
             </div>
             <div class="form-group mt-3">
-                <Field type="password" placeholder="Password" name="password" class="form-control"/>
-                <error-message name="password"></error-message>
+                <Field type="password" placeholder=" Password" name="password"/>
+                <div class="errdiv"><error-message name="password"></error-message></div>
             </div>
             <div class="form-group mt-3">
-                <Field type="password" placeholder="Confirm Password" name="password_confirmation" class="form-control"/>
-                <error-message name="password"></error-message>
+                <Field type="password" placeholder=" Confirm Password" name="password_confirmation"/>
+                <div class="errdiv"><error-message name="password"></error-message></div>
             </div>
-            <input type="submit" value="Register" class="btn btn-primary mt-3">
+            <input type="submit" value="Register" class="mt-3">
         </Form>
     </div>
 </template>
@@ -55,19 +55,50 @@ async function register(userData){
 #regdiv{
     width: 280px;
 }
+input{
+    width: 90%;
+    margin-left: 5%;
+}
 }
 @media screen and (min-width: 720px){
 #regdiv{
     width: 700px;
 }
+input{
+    width: 70%;
+    margin-top: 5px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 5px;
+    margin-left: 15%;
+}
 }
 #regdiv{
     margin-top: 30px;
     margin-bottom: 40px;
-    background-color: #18afa5;
+    background-color: whitesmoke;
     border: 2px solid black;
+    box-shadow:10px 10px #18afa5;
 }
 h3{
+    text-align: center;
+}
+input{
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border: 2px solid black;
+}
+input:focus{
+    background-color: lightblue;
+    border: 3px solid black;
+}
+input[type=submit]{
+    width: 70%;
+    margin-left: 15%;
+}
+.errdiv{
+    margin-left: auto;
+    margin-right: auto;
     text-align: center;
 }
 </style>
